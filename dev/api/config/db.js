c7604067c036a2ba34a784e1c8c18df576cc1988
QUENTIN <36104147+QtnRBT@users.noreply.github.com>
2022-02-14 11:15:37 +0100

    Update db.js

--- dev/api/config/db.js
+++ dev/api/config/db.js
@@ -1,10 +1,10 @@
 let mysql = require("mysql2");
 
 let con = mysql.createConnection({
-  host: "82.66.92.204",
+  host: "soemHost",
   database: "yowl",
-  user: "code",
-  password: "@7y97R$x#BZh_f6V5E=3G@Ap!vV9a8Z-",
+  user: "someUser",
+  password: "somePassword",
   port: "3306",
   multipleStatements: true
 });
